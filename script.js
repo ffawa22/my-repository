1dab49b047ce1e754dc5f356365f6878e5b6c221
FAWAZ <63443371+ffawa22@users.noreply.github.com>
2022-12-04 08:36:38 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btn").addEventListener("click", () => {
-
-    let numRandom = Math.round(Math.random() * 100);
-
-    document.getElementById("num").innerHTML=numRandom;
-    
-})
